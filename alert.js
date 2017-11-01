83a2707bd4e17e614585387563fdee9f5f849994
Kimberly Simmonds <ksimmonds92@gmail.com>
2017-11-01 16:15:23 +0000

    Exercise 3

--- alert.js
+++ alert.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
     
     $("#search").on("click", function(){
-         
         var query = $("#query").val();
-        var url = "https://info2180-lab6-ksimmonds.c9users.io/request.php?q=definition";
-        $.ajax({url:url,
+        var url = "https://info2180-lab6-ksimmonds.c9users.io/request.php?q="+query;
+        $.ajax(url,{
             method: "GET",
         }).done(function(r){
-            alert(r);
+            $("#result").html(r);
         }).fail(function(e){
-            alert(e);
+            $("#result").html(e);
         });
-        
-        
     });
 });
